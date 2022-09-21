b96cc11de79942dae5a1d49155a18df46bc179ce
Jacob Baehr <baehrjacob@gmail.com>
2022-09-21 12:14:03 -0500

    table functionality

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -6,6 +6,47 @@ export default function Table() {
     const [show, setShow] = React.useState(false)
     const [movies, setMovies] = React.useState("")
     const [btnText, setBtnText] = React.useState("Show My Movies and Shows")
+    const [addFormData, setAddFormData] = React.useState({
+        show_id: '',
+        type: '',
+        title: '',
+        director: '',
+        country: '',
+        date_added: '',
+        release_year: '',
+        rating: '',
+        duration: '',
+        listed_in: ''
+    })
+
+    const handleAddFormChange = (event) => {
+        event.preventDefault()
+        const fieldName = event.target.getAttribute('name')
+        const fieldValue = event.target.value
+
+        const newFormData = {...addFormData}
+        newFormData[fieldName] = fieldValue
+
+        setAddFormData(newFormData)
+    }
+
+    const handleAddFormSubmit = (event) => {
+        event.preventDefault()
+
+        const newShow = {
+            show_id: addFormData.show_id,
+            type: addFormData.type,
+            title: addFormData.title,
+            director: addFormData.director,
+            country: addFormData.country,
+            date_added: addFormData.date_added,
+            release_year: addFormData.release_year,
+            rating: addFormData.rating,
+            duration: addFormData.duration,
+            listed_in: addFormData.listed_in
+        }
+    }
+
 
     function handleShow() {
         setShow(prevShow => !prevShow)
@@ -14,17 +55,15 @@ export default function Table() {
                 show ? "Show My Movies and Shows" : "Hide My Movies and Shows"
             )
         })
-        console.log(movies)
 
     }
 
     React.useEffect(() => {
         const LoadShows = async () => {
-            const { shows } = await supabase
+            const { data, error } = await supabase
                 .from('Netflix')
                 .select()
-                .limit(10)
-            setMovies(shows)
+            setMovies(data)
         }
         LoadShows()
     }, [])
@@ -37,21 +76,50 @@ export default function Table() {
                 <table>
                     <thead>
                         <tr>
-                            <th>Name</th>
-                            <th>Address</th>
-                            <th>Phone Number</th>
-                            <th>Email</th>
+                            <th>show id</th>
+                            <th>type</th>
+                            <th>title</th>
+                            <th>director</th>
+                            <th>country</th>
+                            <th>date added</th>
+                            <th>release year</th>
+                            <th>rating</th>
+                            <th>duration</th>
+                            <th>listed in</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Jenny Chan</td>
-                            <td>3 waterfoot road</td>
-                            <td>333-964-5968</td>
-                            <td>jenny@gmail.com</td>
-                        </tr>
+                        {movies.map((movie) => (
+                            <tr>
+                                <td>{movie.show_id}</td>
+                                <td>{movie.type}</td>
+                                <td>{movie.title}</td>
+                                <td>{movie.director}</td>
+                                <td>{movie.country}</td>
+                                <td>{movie.date_added}</td>
+                                <td>{movie.release_year}</td>
+                                <td>{movie.rating}</td>
+                                <td>{movie.duration}</td>
+                                <td>{movie.listed_in}</td>
+                            </tr>
+                        ))}
+                        
                     </tbody>
                 </table>
+                <h2>Add a Movie or Show</h2>
+                <form>
+                    <input type="text" onChange={handleAddFormChange} name="show_id" required="required" placeholder="Enter a show_id"></input>
+                    <input type="text" onChange={handleAddFormChange} name="type" required="required" placeholder="Enter a type"></input>
+                    <input type="text" onChange={handleAddFormChange} name="title" required="required" placeholder="Enter a title"></input>
+                    <input type="text" onChange={handleAddFormChange} name="director" required="required" placeholder="Enter a director"></input>
+                    <input type="text" onChange={handleAddFormChange} name="country" required="required" placeholder="Enter a country"></input>
+                    <input type="text" onChange={handleAddFormChange} name="date_added" required="required" placeholder="Enter a date_added"></input>
+                    <input type="text" onChange={handleAddFormChange} name="release_year" required="required" placeholder="Enter a release_year"></input>
+                    <input type="text" onChange={handleAddFormChange} name="ra ting" required="required" placeholder="Enter a rating"></input>
+                    <input type="text" onChange={handleAddFormChange} name="duration" required="required" placeholder="Enter a duration"></input>
+                    <input type="text" onChange={handleAddFormChange} name="listed_in" required="required" placeholder="Enter a listed_in"></input>
+                    <button type="submit">Add</button>
+                </form>
             </div>}
         </div>
     )
